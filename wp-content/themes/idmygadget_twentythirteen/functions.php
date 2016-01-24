895d2acc3cc5378df731fcfba204a2137446699f
Tom W Hartung <tomwhartung@gmail.com>
2016-01-23 21:18:58 -0700

    Deleted global functions getHeaderHtml and getLogoTitleDescriptionHtml .

--- wp-content/themes/idmygadget_twentythirteen/functions.php
+++ wp-content/themes/idmygadget_twentythirteen/functions.php
@@ -580,60 +580,6 @@ function idmygadget_twentythirteen_check_idMyGadget_install()
 	$jmwsIdMyGadgetCheckPlugin = new JmwsIdMyGadgetCheckPlugin();
 	$jmwsIdMyGadgetCheckPlugin->checkPlugin();
 }
-/**
- * Use the $logoTitleDescription to generate the html for the header
- */
-function getHeaderHtml()
-{
-	global $jmwsIdMyGadget;
-	$logoTitleDescription = '';
-
-	if ( $jmwsIdMyGadget->isInstalled() && $jmwsIdMyGadget->isEnabled() )
-	{
-		$logoTitleDescription = $jmwsIdMyGadget->getLogoTitleDescriptionHtml();
-	}
-	else
-	{
-		$logoTitleDescription = getLogoTitleDescriptionHtml();
-	}
-
-	$headerHtml  = '';
-	$headerHtml .= '<header id="masthead" class="site-header" role="banner" ';
-	$headerHtml .= $jmwsIdMyGadget->jqmDataRole['header'] . ' ';
-	$headerHtml .= $jmwsIdMyGadget->jqmDataThemeAttribute . '>';
-	$headerHtml .= '<div class="site-branding">';
-	$headerHtml .= $logoTitleDescription;
-	$headerHtml .= '</div><!-- .site-branding -->';
-	$headerHtml .= '</header><!-- .site-header -->';
-	return $headerHtml;
-}
-/**
- * If the idMyGadget module is not available we will use this,
- * which is the original twentythirteen code (as of Sept. 2015).
- */
-function getLogoTitleDescriptionHtml()
-{
-	$logoTitleDescription = '';
-	if ( is_front_page() && is_home() )
-	{
-		$logoTitleDescription = '<h1 class="site-title">Exploding!' .
-				'<a href="' . esc_url( home_url('/') ) . '" rel="home">' . $site_name . '</a></h1>';
-	}
-	else
-	{
-		$logoTitleDescription = '<p class="site-title">' .
-				'<a href="' . esc_url( home_url('/') ) . '" rel="home">' . $site_name . '</a></p>';
-	}
-	$description = get_bloginfo( 'description', 'display' );
-	if ( $description || is_customize_preview() )
-	{
-		$logoTitleDescription .= '<p class="site-description">' . $description . '</p>';
-	}
-	// $logoTitleDescription .= '<button class="secondary-toggle">' . _e( 'Menu and widgets', 'twentythirteen' ) . '</button>';
-	$logoTitleDescription .= '<button class="secondary-toggle">Menu and widgets</button>';
-
-	return $logoTitleDescription;
-}
 
 /**
  * Initialize:
