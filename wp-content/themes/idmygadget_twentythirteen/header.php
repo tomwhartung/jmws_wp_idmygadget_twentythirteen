596d0e7e19f5137cd0c0148607b1cf5d1f54df34
Tom W Hartung <tomwhartung@gmail.com>
2016-02-08 21:41:30 -0700

    Changed to add the site-navigation nav bar only if we are not using the jQuery Mobile phone header nav menu feature.

--- wp-content/themes/idmygadget_twentythirteen/header.php
+++ wp-content/themes/idmygadget_twentythirteen/header.php
@@ -53,16 +53,17 @@
 		<?php endif; ?>
 		<header id="masthead" class="site-header" role="banner"
 			<?php echo $jmwsIdMyGadget->jqmDataRole['header'] . ' ' . $jmwsIdMyGadget->jqmDataThemeAttribute; ?> >
-		<?php echo $header_html ?>
-
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
+			<?php echo $header_html ?>
+			<?php if( ! $jmwsIdMyGadgetTwentyThirteenHelper->phoneHeaderNavInTwentyThirteenPage ) : ?>
+				<div id="navbar" class="navbar">
+					<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+						<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
+						<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
+						<?php get_search_form(); ?>
+					</nav><!-- #site-navigation -->
+				</div><!-- #navbar -->
+			<?php endif; ?>
 		</header><!-- #masthead -->
 
 		<div id="main" class="site-main" <?php echo $jmwsIdMyGadget->jqmDataRole['content'] ?>>
