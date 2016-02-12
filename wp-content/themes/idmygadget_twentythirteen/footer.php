19b98bad48584850c4f0f116533691d6337c9b43
Tom W Hartung <tomwhartung@gmail.com>
2016-02-12 00:57:01 -0700

    Fixed the phone footer nav.

--- wp-content/themes/idmygadget_twentythirteen/footer.php
+++ wp-content/themes/idmygadget_twentythirteen/footer.php
@@ -13,22 +13,22 @@ global $jmwsIdMyGadgetTwentyThirteenHelper;
 ?>
 
 		</div><!-- #main -->
-		<footer id="colophon" class="site-footer" role="contentinfo"
-			<?php echo $jmwsIdMyGadget->jqmDataRole['footer'] . ' ' . $jmwsIdMyGadget->jqmDataThemeAttribute ?>>
-			<?php get_sidebar( 'main' ); ?>
-
-			<div class="site-info">
-				<?php do_action( 'twentythirteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+		<?php $original_attributes = 'id="colophon" class="site-footer" role="contentinfo"'; ?>
+		<footer <?php echo $jmwsIdMyGadget->getFooterAttributes($original_attributes); ?>>
+			<?php if( $jmwsIdMyGadgetTwentyThirteenHelper->phoneFooterNavInTwentyThirteenPage ) : ?>
+				<nav data-role="navbar">
+					<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav', 'container' => false) ); ?>
+				</nav>
+			<?php else: ?>
+				<?php get_sidebar( 'main' ); ?>
+				<div class="site-info">
+					<?php do_action( 'twentythirteen_credits' ); ?>
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
+				</div><!-- .site-info -->
+			<?php endif; ?>
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
 	<?php wp_footer(); ?>
-	<?php if( $jmwsIdMyGadgetTwentyThirteenHelper->phoneFooterNavInTwentyThirteenPage ) : ?>
-		<nav data-role="navbar" data-position="fixed">
-			<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav', 'container' => false) ); ?>
-		</nav>
-	<?php endif; ?>
 </body>
 </html>
