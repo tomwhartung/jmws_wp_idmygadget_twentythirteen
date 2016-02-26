ce4bd69ed8fb9d6759c2c703e073334bf75a33fb
Tom W Hartung <tomwhartung@gmail.com>
2016-02-26 12:06:30 -0700

    Removed the Menu button from the header; we are implementing menus in a different way.  As mentioned previously, we are not coding much today, working on organizing content (much like yesterday and the day before, have a lot of ideas to post, we shall see which ones are actually good enough to post...).

--- wp-content/themes/idmygadget_twentythirteen/header.php
+++ wp-content/themes/idmygadget_twentythirteen/header.php
@@ -58,7 +58,6 @@ $site_title_or_name = $jmwsIdMyGadget->getSiteTitleOrName();
 			<?php if( ! $jmwsIdMyGadgetTwentyThirteenHelper->phoneHeaderNavInTwentyThirteenPage ) : ?>
 				<div id="navbar" class="navbar">
 					<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-						<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
 						<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
 						<?php get_search_form(); ?>
