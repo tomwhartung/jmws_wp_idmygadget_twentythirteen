9631fcdd932c96115c0d2a535a31a7f6ac567995
Tom W Hartung <tomwhartung@gmail.com>
2016-03-07 14:19:21 -0700

    Updated sidebar.php to display device-specific widgets as appropriate for the current device.

--- wp-content/themes/idmygadget_twentythirteen/sidebar.php
+++ wp-content/themes/idmygadget_twentythirteen/sidebar.php
@@ -10,12 +10,28 @@
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  */
+global $jmwsIdMyGadget;
+$jmwsIdMyGadget->setIncludeSidebarVariables('sidebar-2');
 
-if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+// if ( is_active_sidebar( 'sidebar-2' ) ) :
+if ( $jmwsIdMyGadget->includeSidebar ) : ?>
 	<div id="tertiary" class="sidebar-container" role="complementary">
 		<div class="sidebar-inner">
 			<div class="widget-area">
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+				<?php if ( $jmwsIdMyGadget->includeSidebarPhones ) : ?>
+					<div id="sidebar-phones">
+						<?php dynamic_sidebar( 'sidebar-phones' ); ?>
+					</div>
+				<?php elseif ( $jmwsIdMyGadget->includeSidebarTablets ) : ?>
+					<div id="sidebar-tablets">
+						<?php dynamic_sidebar( 'sidebar-tablets' ); ?>
+					</div>
+				<?php elseif ( $jmwsIdMyGadget->includeSidebarDesktops ) : ?>
+					<div id="sidebar-desktops">
+						<?php dynamic_sidebar( 'sidebar-desktops' ); ?>
+					</div>
+				<?php endif; ?>
 			</div><!-- .widget-area -->
 		</div><!-- .sidebar-inner -->
 	</div><!-- #tertiary -->
